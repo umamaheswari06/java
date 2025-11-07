7629465a9dcbdc7bbaf9457cb4e5c56b02f09327
umamaheswari06 <umakalai0808@gmail.com>
2025-11-07 17:23:52 +0530

    Updated If.java with new method

--- If.java
+++ If.java
@@ -6,7 +6,7 @@ public class If {
         // if statement : performs a block of code if its condition is true
 
         Scanner scan = new Scanner(System.in);
-        System.out.print("Enter your age:");
+       /*  System.out.print("Enter your age:");
          int age = scan.nextInt();
 
          scan.nextLine();
@@ -38,8 +38,52 @@ public class If {
            }
         else{
             System.out.println("Your are a child!");
+        }*/
+
+        //nested if
+        /*if(){
+            if() {
+                
+            }
+            else if() {
+                
+            }
+            else{
+
+            }
         }
+            */
+        System.out.print("Enter ticket base price: ");
+        double price = scan.nextDouble();
+
+        System.out.print("Are you a student? (true/false): ");
+        boolean isStudent = scan.nextBoolean();
+
+        System.out.print("Are you a senior citizen? (true/false): ");
+        boolean isSenior = scan.nextBoolean();
+            if(isStudent){
+                if (isSenior){
+                System.out.println("You get a senior discount of 20%");
+                System.out.println("You get a Student discount of 10%");
+                price*=0.7;
+                }
+            else{
+              System.out.println("You get a Student discount of 10%");
+            price*=0.9;
+            }
+        }
+        else{
+            if(isSenior){
+                System.out.println("You get a senior discount of 20%");
+                price*=0.8;
+            }
+            else{
+                price*=1;
+            }
+        }
+        System.out.printf("the price of a ticket is : $%.2f" , price);
         scan.close();
 
     }
 }
+
